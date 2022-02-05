6c1fc5244402a44141ca63b639fdb8695397ee44
YasineNifa <yasine.nifa@gmail.com>
2022-02-05 22:41:03 +0100

    Connect your Metamask Wallet with the application
    Signed-off-by: YasineNifa <yasine.nifa@gmail.com>

---
+++ opensea-blockchain-clone/pages/_app.js
@@ -0,0 +1,22 @@
+import '../styles/globals.css'
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks'
+
+
+// The chain ID represents the Rainkiby network
+// the injected connector is a web3 connection method used by Metamask
+const supportedChainIds = [4]
+const connectors = {
+  injected:{},
+}
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThirdwebWeb3Provider
+    supportedChainIds={supportedChainIds}
+    connectors={connectors}
+    >
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  )
+}
+
+export default MyApp
